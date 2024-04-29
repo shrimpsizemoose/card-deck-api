26d6bdf5d556ac1c1246087603e4d6528f8759f3
Igor Mosyagin <408698+cra@users.noreply.github.com>
2024-04-29 10:43:43 +0200

    Remove map with locks from InMemoryStorage

--- storage/storage.go
+++ storage/storage.go
@@ -19,39 +19,26 @@ type DeckStorage interface {
 
 type InMemoryStorage struct {
 	decks map[uuid.UUID]deck.Deck
-	locks map[uuid.UUID]*sync.RWMutex
 	mu    sync.Mutex
 }
 
 func NewInMemoryStorage() *InMemoryStorage {
 	return &InMemoryStorage{
 		decks: make(map[uuid.UUID]deck.Deck),
-		locks: make(map[uuid.UUID]*sync.RWMutex),
 	}
 }
 
 func (s *InMemoryStorage) SaveDeck(ctx context.Context, d deck.Deck) error {
 	s.mu.Lock()
-	if _, ok := s.locks[d.ID]; !ok {
-		s.locks[d.ID] = &sync.RWMutex{}
-	}
-	s.locks[d.ID].Lock()
+	defer s.mu.Unlock()
+
 	s.decks[d.ID] = d
-	s.locks[d.ID].Unlock()
-	s.mu.Unlock()
 	return nil
 }
 
 func (s *InMemoryStorage) GetDeck(ctx context.Context, id uuid.UUID) (deck.Deck, bool) {
 	s.mu.Lock()
-	lock, ok := s.locks[id]
-	if !ok {
-		s.mu.Unlock()
-		return deck.Deck{}, false
-	}
-	lock.RLock()
-	defer lock.RUnlock()
-	s.mu.Unlock()
+	defer s.mu.Unlock()
 
 	d, found := s.decks[id]
 	return d, found
@@ -59,30 +46,15 @@ func (s *InMemoryStorage) GetDeck(ctx context.Context, id uuid.UUID) (deck.Deck,
 
 func (s *InMemoryStorage) DeleteDeck(ctx context.Context, id uuid.UUID) error {
 	s.mu.Lock()
-	lock, ok := s.locks[id]
-	if !ok {
-		s.mu.Unlock()
-		return fmt.Errorf("deck not found")
-	}
-	lock.Lock()
+	defer s.mu.Unlock()
+
 	delete(s.decks, id)
-	delete(s.locks, id)
-	lock.Unlock()
-	s.mu.Unlock()
 	return nil
 }
 
 func (s *InMemoryStorage) UpdateDeck(ctx context.Context, d deck.Deck) error {
 	s.mu.Lock()
-	lock, ok := s.locks[d.ID]
-	if !ok {
-		s.mu.Unlock()
-		return fmt.Errorf("deck with id=%v not found", d.ID)
-	}
-
-	lock.Lock()
-	defer lock.Unlock()
-	s.mu.Unlock()
+	defer s.mu.Unlock()
 
 	if _, found := s.decks[d.ID]; !found {
 		return fmt.Errorf("deck with id=%v not found", d.ID)
